77723d1dc75c8e35dee671979ba64804533af164
Kamilla Araújo dos Santos <kamillassantos456@gmail.com>
2025-08-25 16:26:04 -0300

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,9 @@
+#atividade_senha
+senha = ""
+
+while senha != "python123":
+    senha = input("Digite a senha: ")
+    if senha != "python123":
+        print ("senha incorreta!")
+
+print ("Senha correta!")
