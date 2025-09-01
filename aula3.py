af40e9b04e8cde970cc80e8dadafd427d94c9318
Kamilla Araújo dos Santos <kamillassantos456@gmail.com>
2025-09-01 14:49:28 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,7 @@
+#tuplas
+cores= ("rosa" , "azul", "verde", "amarelo", "vermelho")
+if "azul" in cores:
+    print("azul está na tupla")
+else:
+    ("azul não está na tupla")
+print(cores[1],cores[4])
